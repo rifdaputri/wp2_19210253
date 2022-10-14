775cbf241995b90a17571a7667754d66c112550f
rifdaputri <ripdaptr@gmail.com>
2022-10-14 14:48:41 +0700

    First Commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+	
+	public function index()
+	{
+		echo "<h1>Perkenalan</h1>";
+		echo "
+		Nama saya Rifda <br>
+		Saya tinggal di dunya";
+	}
+}
